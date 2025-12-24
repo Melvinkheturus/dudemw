b4d63c90197f60583cea3a49c1abdd45d34a6f27
Manikandan <smk.manikandan.dev@gmail.com>
2025-12-24 13:39:30 +0530

    Fix Hostinger build: Harden TypeScript config and force Node 20

---
+++ src/types/global.d.ts
@@ -0,0 +1,5 @@
+/// <reference lib="es2020" />
+/// <reference lib="dom" />
+
+// Force Promise and other ES2015+ types to be available
+// This ensures TypeScript has access to modern types even if tsconfig is partially ignored
